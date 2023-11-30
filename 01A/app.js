9d24fb90749ed114e49c5fa63f4fe066370df07a
Jona <36910@ma-web.nl>
2023-11-30 12:28:27 +0100

    edit

---
+++ 01A/app.js
@@ -0,0 +1,27 @@
+class App
+{
+
+    runApplication()
+    {
+        console.log("hello world!")
+        
+        let eenVariableNaam = "hello"
+        let appNaam = "demoApp"
+        let versienummer = 0.2
+        let versiedatum = 30-11-2023
+        let autheur = "Jona"
+        let copyright = "Jona"
+        let distributeur = "Jona"
+        let darkmode = false
+
+        console.log(appNaam);
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+
+    }
+}
+let app = new App();
+app.runApplication();
