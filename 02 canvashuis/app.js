9a135a7a6352b6b48135b0b8bae4882708396edd
Jona <36910@ma-web.nl>
2023-12-05 13:00:48 +0100

    02 canvashuis

---
+++ 02 canvashuis/app.js
@@ -0,0 +1,18 @@
+class App
+{
+    runApplication()
+    {
+        this. greeting ="Starting up";
+
+        console.log("hello world!")
+
+        let canvas = document.getElementById("canvasId");
+
+        console.log(canvas)
+
+        let g = canvas.getContext("2d");
+        g.fillRect(0,0,10,10);
+    }
+}
+let app = new App();
+app.runApplication();
