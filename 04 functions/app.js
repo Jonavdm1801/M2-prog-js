9e5a604ad3be4f26f97a6813898728c56f766227
Jona <36910@ma-web.nl>
2024-01-30 12:23:42 +0100

    04 03

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -10,27 +10,43 @@ class App
 let app = new App();
 app.runApplication();
 
-function GlobalFunction()
+function Result()
 {
-    console.log("global");
-    console.log("y");
+      let resultaat =1;
+      return resultaat;
 }
 
-GlobalFunction();
+let hetResult = Result();
+console.log(hetResult);
+console.log(Result());
 
+function ax2bcWiskunde(x, a, b, c)
+{
+        
 
-function aanroep()
+    let y = (a*(x*x) )+ (b*x) +c;  
+    return y; 
+}
+
+ax2bcWiskunde(2, 5, 8, 1);
+let som = ax2bcWiskunde(2, 5, 8, 1);
+console.log(som);
+
+    let y1 = ax2bcWiskunde(9,3,4,89);
+    console.log(y1);
+
+    let y2 = ax2bcWiskunde(3,6,5,45);
+    console.log(y2);
+
+    let y3 = ax2bcWiskunde(7,1,2,13);
+    console.log(y3);
+
+function decimaal(u, h, l)
 {
 
+    let logY = Math.log(l) +  h + Math.pow(u,2);
+    return logY;
 }
- aanroep();
-
- function argumenten(shoutout)
-{
-   console.log("do you want a shoutout");
-   console.log(shoutout);
-}
- argumenten("Super Shout out");
-
- argumenten("Super mario kamehameha");
- argumenten("Souls games are pretty dang cool");
+ decimaal(9,8,15);
+ let uitkomst = decimaal(9,8,15);
+ console.log(uitkomst);
