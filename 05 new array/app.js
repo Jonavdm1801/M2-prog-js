790c800bdc7f5c667d899a9e3089229adf3994ee
Jona <36910@ma-web.nl>
2024-02-01 12:12:16 +0100

    05 02

--- 05 new array/app.js
+++ 05 new array/app.js
@@ -4,32 +4,18 @@ class App
     {
      
         console.log("Hello world!");
-        let arr = ["artiest1", "Artiest2", "etc"]
+        let arr = ["Kenderick lamar", "Ed Sheeran", "Snoop Dogg"]
         console.log(arr)
 
         for (let i = 0; i < arr.length; i++) {
             const element = arr[i];
             console.log(i + ": "+ element)
         }
-
-        for (let i = 0; i < 5; i++) {
-            const element = arr[i];
-            arr.push("bob dylan")
-            arr.push("prince")
-            console.log(i + ": "+ element)
-        }
-
-        for (let i = 0; i < 5; i++) {
-            const element = arr[i];
-            arr.push("bob dylan")
-            arr.push("prince")
-            console.log(i + ": "+ element)
-            let indexToRemove = arr.indexOf("bob dylan")
-            let indexToRemove1 = arr.indexOf("prince")
-            arr.splice(indexToRemove,1)
-            arr.splice(indexToRemove1,5)
-            arr.push("big bad voodoo daddy")
-            arr.push("megadeth")  
+        let nummers = [2,5,7]
+        console.log(nummers)
+        for (let i = 0; i < nummers.length; i++) {  
+            const element = nummers[i];
+            console.log(1 + element)
         }
     }
 }
